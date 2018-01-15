050b3f0ddea6a4c9611699cd0427c08c446ed848
KayTeo <keith_teo@outlook.sg>
2018-01-15 20:53:02 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<center>SWAP Website E-Commerece</center>
+<form method="post" action="search.php">
+<input type="text" name="keyword"><br>
+<input type="submit" value="Search"/>
+</form>
+
+<br> <a href='retailerlogin.php'> Retailer Login </a> </br>
+<br> <a href='customerlogin.php'> Customer Login </a> </br>
+
+<img src="images/potato.png" alt="potato" height="300" width="300">
+<img src="images/doge.jpg" alt="doge" height="300" width="300">
+
+</body>
+</html>
