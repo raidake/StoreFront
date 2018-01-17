ae3a4128742cc1ffbd7779b91cb27f9ca92368f3
raidake <33020827+raidake@users.noreply.github.com>
2018-01-17 12:05:28 +0800

    Delete db.php

--- db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mysqli = mysqli_connect("localhost","root","","employee"); //connect to database
-if (!$mysqli){
-	die('Could not connect: ' . mysqli_connect_errno()); //return error is connect fail
-}
