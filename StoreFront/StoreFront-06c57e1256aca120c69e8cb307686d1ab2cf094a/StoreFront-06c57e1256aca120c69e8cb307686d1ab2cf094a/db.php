e7934f893a74fecc0f36d09970da5d59a51e30ba
raidake <33020827+raidake@users.noreply.github.com>
2018-01-17 12:10:38 +0800

    Delete db.php

--- StoreFront/StoreFront-06c57e1256aca120c69e8cb307686d1ab2cf094a/StoreFront-06c57e1256aca120c69e8cb307686d1ab2cf094a/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mysqli = mysqli_connect("localhost","root","","main"); //connect to database
-if (!$mysqli){
-	die('Could not connect: ' . mysqli_connect_errno()); //return error is connect fail
-}
