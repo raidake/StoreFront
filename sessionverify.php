370ab33dc8d23ac017348baa2c5e66415c0f4dd0
KayTeo <keith_teo@outlook.sg>
2018-01-18 21:18:00 +0800

    Add files via upload

--- sessionverify.php
+++ sessionverify.php
@@ -3,10 +3,11 @@
 $con=mysqli_connect("localhost","root","","employee");		
 session_start();
 if(isset($_SESSION['username'])){
+$username=$_SESSION['username'];
 $result=$con->query("SELECT * from employee WHERE username='$username'");
 $user=$result->fetch_assoc();
 	if ( $_SESSION['hash'] != $user['hash']){
-		$_SESSION['message']="Access denied. Invalid credentials";
+		$_SESSION['message']="Access denied. Invalid credentials.";
 		header("location:error.php");
 	}
 }
