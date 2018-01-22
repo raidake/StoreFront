c03c95bb9c7a57c323672228fa52cae68f580761
EthanHeng <33022071+EthanHeng@users.noreply.github.com>
2018-01-22 14:52:28 +0800

    Logs login of employee

---
+++ StoreFront/Administrator/LoginSuccessful.php
@@ -0,0 +1,27 @@
+<?php
+
+$con = mysqli_connect("localhost","root","","auditor database"); //connect to database
+if (!$con){
+	die('Could not connect: ' . mysqli_connect_errno()); //return error is connect fail
+}
+
+date_default_timezone_set('Asia/Singapore');
+$timestamp = date("F j, Y, g:i a");
+
+$date = date("Y-m-d");
+$time = date("H:i:s");
+
+$logging = "Username: '$username' , logged in at, time: '$timestamp'"; 
+
+$query= $con->prepare("INSERT INTO `audit_logs`(`date`,`time`,`log_details`) VALUES(?,?,?)");
+
+$query->bind_param('sss',$date,$time,$logging);
+if ($query->execute()){  //execute query
+  echo "Query executed.";
+}else{
+  echo "Error executing query.";
+}
+  
+
+
+?>
