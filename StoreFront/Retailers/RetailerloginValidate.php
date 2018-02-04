52f10c7508716252f20ee7f539a07a52baf88183
KayTeo <keith_teo@outlook.sg>
2018-02-04 18:08:21 +0800

    Add files via upload

--- StoreFront/Retailers/RetailerloginValidate.php
+++ StoreFront/Retailers/RetailerloginValidate.php
@@ -48,7 +48,7 @@ else // User exists
 		$_SESSION['hash'] = $user['hash'];
 		$_SESSION['retails_ID']=$user['retails_ID'];
 		//Set session to tell account is customer or retailer
-		$_SESSION['company_name']=$user['company_name'];
+		$_SESSION['company_name']=$user['company_Name'];
 		$_SESSION['email'] = $user['e-mail'];
 		$_SESSION['contact']=$user['phone_Number'];
 		$_SESSION['description']=$user['description'];
