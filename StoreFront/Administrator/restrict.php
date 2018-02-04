70bc62b56df1587d560c627d5c17e617f96c0304
KayTeo <keith_teo@outlook.sg>
2018-02-04 18:29:54 +0800

    Update restrict.php

--- StoreFront/Administrator/restrict.php
+++ StoreFront/Administrator/restrict.php
@@ -7,7 +7,7 @@ date_default_timezone_set("Singapore");
 
 //Check if time is between 9 pmand 7 am and deny access if it is
 //date("H") returns the hour in 24 hour time format
-if(date("H")>20 && date("H")<7){
+if(date("H")>21 && date("H")<7){
 	session_start();
 	$_SESSION['message']="We are currently closed. Our operating hours are from 7 am to 9 pm.";
 	header("location: error.php");
